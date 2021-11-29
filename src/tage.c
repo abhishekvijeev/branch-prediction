b747902cd837a7eefa14a528899bd9392b8f4fe2
Abhishek Vijeev <abhishek.vijeev@gmail.com>
2021-11-29 21:21:15 +0530

    Implement tage_train_predictor() function as well as helper functions needed to update saturation counters and compressed history
    Signed-off-by: Abhishek Vijeev <abhishek.vijeev@gmail.com>

--- src/tage.c
+++ src/tage.c
@@ -149,7 +149,125 @@ tage_make_prediction(uint32_t pc)
 }
 
 void
+tage_update_counter(uint8_t *counter, uint8_t n_bits, uint8_t direction) {
+  uint8_t counter_max = (1 << n_bits) - 1;
+  if (direction == TAKEN) {
+    if ((*counter) < counter_max) {
+      (*counter)++;
+    }
+  }
+  else if (direction == NOTTAKEN) {
+    if ((*counter) > 0) {
+      (*counter)--;
+    }
+  }
+}
+
+void
+tage_update_compressed_history(uint16_t *compressed_hist, uint8_t compressed_hist_bits, uint8_t predictor_num)
+{
+  uint16_t predictor_hist_bits = tagged_predictor_history_lengths[predictor_num];
+  uint16_t split_point = predictor_hist_bits % compressed_hist_bits;
+  LOG("\tsplit_point: %u", split_point);
+  LOG("\tpredictor_hist_bits: %u", predictor_hist_bits);
+  LOG("\tglobal_hist_bit: %d", global_history_register[predictor_hist_bits]);
+
+  (*compressed_hist) = ((*compressed_hist) << 1) | global_history_register[0];
+  LOG("\tcompressed_hist: %u", (*compressed_hist));
+  (*compressed_hist) ^= (global_history_register[predictor_hist_bits] << split_point);
+  LOG("\tcompressed_hist: %u", (*compressed_hist));
+  (*compressed_hist) ^= ((*compressed_hist) >> compressed_hist_bits);
+  LOG("\tcompressed_hist: %u", (*compressed_hist));
+  (*compressed_hist) &= ((1 << compressed_hist_bits) - 1);
+  LOG("\tcompressed_hist: %u\n", (*compressed_hist));
+}
+
+void
 tage_train_predictor(uint32_t pc, uint8_t outcome)
 {
+  // TODO: Reset column of u every 256k iterations
+
+  bool allocate_entry = (final_prediction != outcome) && (provider_component > 0);
+
+  if (allocate_entry) {
+    // Count the number of predictors that have their useless bit reset
+    bool found_useless = false;
+    uint8_t useless_count = 0;
+    for (int i = provider_component - 1; i >= 0; i--) {
+      uint16_t index = tagged_predictor_cached_indices[i];
+      if (tagged_predictors[i].predictor_table[index].u == 0) {
+        found_useless = true;
+        useless_count++;
+      }
+    }
+
+    if (found_useless) {
+      // Find all predictors that have their useless bit reset
+      uint8_t *useless_predictors = (uint8_t *)calloc(useless_count, sizeof(uint8_t));
+      for (int i = provider_component - 1, j = 0; i >= 0; i--) {
+        uint16_t index = tagged_predictor_cached_indices[i];
+        if (tagged_predictors[i].predictor_table[index].u == 0) {
+          useless_predictors[j] = i;
+          j++;
+        }
+      }
+
+      // Randomly pick one of the above found predictors and allocate a new entry
+      int random = rand() % useless_count;
+      uint8_t predictor_to_allocate = useless_predictors[random];
+      uint16_t index = tagged_predictor_cached_indices[predictor_to_allocate];
+      if (outcome == TAKEN) {
+        tagged_predictors[predictor_to_allocate].predictor_table[index].pred = 4;
+      }
+      else {
+        tagged_predictors[predictor_to_allocate].predictor_table[index].pred = 3;
+      }
+      tagged_predictors[predictor_to_allocate].predictor_table[index].tag = tagged_predictor_cached_tags[predictor_to_allocate];
+      tagged_predictors[predictor_to_allocate].predictor_table[index].u = 0;
+      free(useless_predictors);
+    }
+    else {
+      // Decrement all useful counters
+      for (int i = provider_component - 1; i >= 0; i--) {
+        uint16_t index = tagged_predictor_cached_indices[i];
+        tage_update_counter(&tagged_predictors[i].predictor_table[index].u, 2, NOTTAKEN);
+      }
+    }
+  }
+
+  // Update the table that gave the final prediction 
+  if (provider_found) {
+    uint16_t index = tagged_predictor_cached_indices[provider_component];
+    tage_update_counter(&(tagged_predictors[provider_component].predictor_table[index].pred), 3, outcome);
+  }
+  else {
+    uint16_t index = tage_compute_base_predictor_index(pc);
+    tage_update_counter(&(base_predictor[index].pred), 2, outcome);
+  }
+
+  // This situation only arises if we obtained a hit in one of the tagged predictors
+  // Hence, 'provider_component' is guaranteed to have a valid value
+  if (final_prediction != alternate_prediction) {
+    uint16_t index = tagged_predictor_cached_indices[provider_component];
+    tage_update_counter(&(tagged_predictors[provider_component].predictor_table[index].u), 2, (final_prediction == outcome));
+  }
+
+  // Update global history
+  for(int i = MAX_HISTORY_BITS - 1 ; i > 0 ; i--){
+      global_history_register[i] = global_history_register[i - 1];
+  }
+  global_history_register[0] = outcome;
+
+  // Update compressed histories
+  for (int i = 0; i < NUM_TAGGED_PREDICTORS; i++) {
+    uint16_t index = tagged_predictor_cached_indices[i];
+    tage_update_compressed_history(&(tagged_predictors[i].index_csr), TAGGED_PREDICTOR_INDEX_BITS, i);
+    LOG("\tpredictor: %d, index_csr: %u", i, tagged_predictors[i].index_csr);
+    tage_update_compressed_history(&(tagged_predictors[i].tag_csr1), TAGGED_PREDICTOR_TAG_BITS, i);
+    LOG("\tpredictor: %d, tag_csr1: %u", i, tagged_predictors[i].tag_csr1);
+    tage_update_compressed_history(&(tagged_predictors[i].tag_csr2), (TAGGED_PREDICTOR_TAG_BITS - 1), i);
+    LOG("\tpredictor: %d, tag_csr2: %u", i, tagged_predictors[i].tag_csr2);
+  }
+
   pc_count++;
 }
